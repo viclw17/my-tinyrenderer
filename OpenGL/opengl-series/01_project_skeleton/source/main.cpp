45cc1208e3d2e81e8bbb976ffb020d792ba9b916
Victor Li <viclw17@gmail.com>
2019-05-11 00:55:27 -0700

    Debug done!!!

--- OpenGL/opengl-series/01_project_skeleton/source/main.cpp
+++ OpenGL/opengl-series/01_project_skeleton/source/main.cpp
@@ -1,33 +1,37 @@
 #include "platform.hpp"
 
 #include <GL/glew.h>
+//#include <glew.c> // fix - Undefined symbols for architecture x86_64: "___glewAttachShader", referenced from:...
+//no need if added in build phase
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+
 // standard c++ libraries
 #include <cassert>
 #include <iostream>
 #include <stdexcept>
 #include <cmath>
+
 // tdogl classes
 #include "Program.h"
 
-const glm::vec2 SCREEN_SIZE(600,600);
-const std::string vs = "/Users/wei_li/Git/my-tinyrenderer/OpenGL/OpenGL/OpenGL/01_project_skeleton/resources/vertex-shader.txt";
-const std::string fs = "/Users/wei_li/Git/my-tinyrenderer/OpenGL/OpenGL/OpenGL/01_project_skeleton/resources/fragment-shader.txt";
+// constants
+const glm::vec2 SCREEN_SIZE(400,400);
 
+// globals
 GLFWwindow* gWindow = NULL;
 GLuint gVAO = 0;
 GLuint gVBO = 0;
 tdogl::Program* gProgram = NULL;
 
 
-//ResourcePath("vertex-shader.txt"),GL_VERTEX_SHADER));
-//ResourcePath("fragment-shader.txt"),GL_FRAGMENT_SHADER));
-
+// loads the vertex shader and fragment shader, and links them to make the global gProgram
 static void LoadShaders(){
     std::vector<tdogl::Shader> shaders;
-    shaders.push_back(tdogl::Shader::shaderFromFile(vs, GL_VERTEX_SHADER));
-    shaders.push_back(tdogl::Shader::shaderFromFile(fs, GL_FRAGMENT_SHADER));
+    shaders.push_back(tdogl::Shader::shaderFromFile(
+    ResourcePath("vertex-shader.txt"), GL_VERTEX_SHADER));
+    shaders.push_back(tdogl::Shader::shaderFromFile(
+    ResourcePath("fragment-shader.txt"), GL_FRAGMENT_SHADER));
 	gProgram = new tdogl::Program(shaders);
 }
 
@@ -76,19 +80,35 @@ static void Render(){
 // }
 
 void AppMain(){
-    // glfwSetErrorCallback(OnError);
-    // if(!glfwInit())
-    //     throw std::runtime_error("glfwInit failed");
+    // initialise GLFW
+    glfwInit(); //victor, simple
+//    glfwSetErrorCallback(OnError);
+//    if(!glfwInit())
+//        throw std::runtime_error("glfwInit failed");
 
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,2);
+//    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	gWindow = glfwCreateWindow((int)SCREEN_SIZE.x,(int)SCREEN_SIZE.y,"test",NULL,NULL);
 
-    // if(!gWindow)
-    //     throw std::runtime_error("glfwCreateWindow failed.");
+//    if(!gWindow)
+//        throw std::runtime_error("glfwCreateWindow failed. Can your hardware handle OpenGL 3.2?");
+    
+    // Victor
+    // Receiving input events
+//    glfwSetKeyCallback(gWindow, key_callback);
+    
+    // GLFW settings
+    // Before you can use the OpenGL API, you must have a current OpenGL context.
+    glfwMakeContextCurrent(gWindow);
+    
+    // initialise GLEW
+    glewExperimental = GL_TRUE; //stops glew crashing on OSX :-/
+    if(glewInit() != GLEW_OK)
+        throw std::runtime_error("glewInit failed");
 
     LoadShaders();
     LoadTriangle();
